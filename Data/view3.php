4a8419a178b4c666ee65e6153872005aa095f2d5
100750029 <80918481+100750029@users.noreply.github.com>
2021-11-28 20:39:39 -0500

    Add files via upload

---
+++ Data/view3.php
@@ -0,0 +1,31 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$db = "FinalProject";
+
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+
+    $sql = "SELECT * FROM Courses WHERE CourseDelivery ='In Person' AND Capacity > 100";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table><tr><th>CourseID</th><th>CourseName</th><th>CourseDelivery</th><th>Capacity</th><th>ExamDates</th><th>InstructorID</th></tr>";
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["CourseID"]."</td><td>".$row["CourseName"]."</td><td>".$row["CourseDelivery"]."</td><td>".$row["Capacity"]."</td><td>".$row["ExamDates"]."</td><td>".$row["InstructorID"]."</td></tr>";
+        }
+            echo "</table>";
+    }else {
+        echo "0 results";
+    }
+
+mysqli_close($conn);
+?>
